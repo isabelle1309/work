dd795898d7e7456d841b4739d44935497631a72c
Isabelle <miau>
2026-06-16 16:23:29 +0200

    add export db

--- script/modules/firebase.js
+++ script/modules/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs }
-    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCl4Tr2Cz1Q5-S4yw066CAKzIv2wUwDpvk",
@@ -13,3 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+export { db };
